ee8db1058b280d9335dd9f398816af9204caa9bb
Shamy <ahmed.alshamy@learningpool.com>
2023-06-20 13:01:21 +0100

    more initiale code changes

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,16 @@
 import pygame
+import sys
+import time
+import pygame.locals import *
+
+# Declare global variables for the 'X' and 'O' symbols.
+XO = 'X'
+
+winner = None
+
+draw = None
+
+width = 400
+height = 400
+
+background_color = (255,255,255)
